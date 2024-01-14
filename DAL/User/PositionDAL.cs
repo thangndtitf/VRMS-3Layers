7f06e8d29d4598ee3f401c9b3e92308c15c99259
thangndtitf <thangndtitf@gmail.com>
2024-01-14 12:26:47 +0700

    no message

--- DAL/User/PositionDAL.cs
+++ DAL/User/PositionDAL.cs
@@ -1,6 +1,26 @@
-﻿namespace VRMS_3layers.DAL.User
+﻿using VRMS_3Layers.Models.User;
+
+namespace VRMS_3layers.DAL.User
 {
     public class PositionDAL
     {
+        public List<MdPosition> getListPosition()
+        {
+            List<MdPosition> listPositions = null;
+
+            using (var _userDbContext = new UserDbContextcs())
+            {
+                try
+                {
+                    listPositions = _userDbContext.MdPositions.Where<MdPosition>(p => p.Isdeleted != 0).ToList();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            return listPositions;
+        }
     }
 }
