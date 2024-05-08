28f6c2eaa13d455d68bdafcb6c06b2f214eb4760
thangndtitf <thangndtitf@gmail.com>
2024-05-08 16:10:17 +0700

    Thang commit 08052024 3

---
+++ BLL/User/UserBLL.cs
@@ -0,0 +1,94 @@
+﻿using VRMS_3layers.DAL.User;
+using VRMS_3layers.Models.ResultObj;
+using VRMS_3Layers.Models;
+
+namespace VRMS_3Layers.BLL.User
+{
+	public class UserBLL
+	{
+
+		/*
+		 Hàm dùng để get List User còn hiệu lực 
+		 */
+		public static ResultObject GetListUser()
+		{
+			ResultObject result = new ResultObject();
+			List<MdUser> listUser = UserDAL.GetListUsers();
+			if(listUser == null)
+			{
+                result.isError = true;
+                result.message = "Get List User Error ";
+				result.messageDetail = "List User is null";
+                result.dataObject = null;
+            }
+			if(listUser.Count <= 0 )
+			{
+                result.isError = true;
+                result.message = "Get List User Error ";
+                result.messageDetail = "List User is Empty";
+                result.dataObject = null;
+            }
+			else
+			{
+				for(int i = 0; i<= listUser.Count - 2; i++)
+				{
+					for (int j = 0; j < listUser.Count - 2; j++)
+					{
+						MdUser userTemp = listUser[j + 1];
+						listUser[j + 1] = listUser[j];
+						listUser[j] = userTemp;
+					}
+				}
+
+                result.isError = false;
+                result.message = "Get List User Success ";
+                result.messageDetail = string.Empty; ;
+                result.dataObject = listUser;
+            }
+			return result;
+		}
+
+		/*
+		 Hàm để get User by ID
+		*/
+		public static ResultObject GetUserById(decimal userName)
+		{
+			ResultObject result = new ResultObject();
+			MdUser user = UserDAL.GetUserById(userName);
+
+			if(user == null)
+			{
+                result.isError = true;
+                result.message = "Get User with ID :" + userName + " Error!!!";
+                result.messageDetail = "User " + userName + " is Empty";
+                result.dataObject = null;
+            }
+			else if (user.Isdeleted == 1 || user.Isactived == 0)
+			{
+                result.isError = true;
+                result.message = "Get User with ID :" + userName + " Error!!!";
+                result.messageDetail = "User " + userName + " is Unactived";
+                result.dataObject = null;
+            }
+			else
+			{
+				result.isError = false;
+                result.message = "Get User " + userName + "Success ";
+                result.messageDetail = string.Empty; ;
+                result.dataObject = user;
+            }
+			return result;
+		}
+
+		/*
+		 Hàm để insert mới dữ liệu User 
+		 */
+		//public static ResultObject InsertNewUser(MdUser newUser)
+		//{
+  //          ResultObject result = new ResultObject();
+		//	MdUser insertUser = UserDAL.InsertUser(newUser);
+
+  //      }
+    }
+}
+
---
+++ Models/MdUpdateUser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace VRMS_3Layers.Models
+{
+	public class MdUpdateUser
+	{
+        /// <summary>
+        /// Mã nhân viên 
+        /// </summary>
+        public decimal Username { get; set; }
+
+        /// <summary>
+        /// Tên đầy đủ của nhân viên 
+        /// </summary>
+        public string Userfullname { get; set; } = null!;
+
+        /// <summary>
+        /// Ngày vào làm chính thức của nhân viên 
+        /// </summary>
+        public DateOnly Insitedate { get; set; }
+
+        /// <summary>
+        /// Ngày nghỉ chính thức của nhân viên 
+        /// </summary>
+        public DateOnly? Outdate { get; set; }
+
+        /// <summary>
+        /// Đã được kích hoạt 
+        /// </summary>
+        public short? Isactived { get; set; }
+
+        /// <summary>
+        /// Ngày kích hoạt 
+        /// </summary>
+        public DateOnly? Activeddate { get; set; }
+
+        /// <summary>
+        /// Số CCCD/ CMND
+        /// </summary>
+        public string Usercardid { get; set; } = null!;
+
+        /// <summary>
+        /// Mô tả 
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Địa chỉ đầy đủ của NV 
+        /// </summary>
+        public string Userfulladdress { get; set; } = null!;
+
+        /// <summary>
+        /// Mã vị trí 
+        /// </summary>
+        public decimal Positionid { get; set; }
+
+        /// <summary>
+        /// Mã phòng ban 
+        /// </summary>
+        public decimal Departmentid { get; set; }
+    }
+}
+
